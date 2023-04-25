c82062fb59dcf752a50caf9e7d215679503e4d40
xiaLotus <116232069+xiaLotus@users.noreply.github.com>
2023-04-25 09:15:20 +0800

    move

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
 @app.get("/")
 def read_root():
     return {"Hello" : "測試"}
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
+
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.dict()}
+
+@app.delete("/items/{item_id}")
+async def read_item(item_id: str):
+    return {"item_id": item_id}
